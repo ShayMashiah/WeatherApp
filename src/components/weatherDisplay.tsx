3aec0e4dc21ea83c627b29413694ee6660bc0c2e
ShayMashiah <shay2908@gmail.com>
2025-05-18 16:49:59 +0300

    feat: adding styling using css and teilwinds

--- src/components/weatherDisplay.tsx
+++ src/components/weatherDisplay.tsx
@@ -8,14 +8,22 @@ const WeatherDisplay: React.FC<WeatherDisplayProps> = ({ weatherData }) => {
   if (!weatherData) return null;
 
   return (
-    <div className="text-center mt-6">
-      <h2>{weatherData.location.name}, {weatherData.location.country}</h2>
-      <p>{weatherData.current.temp_c}°C</p>
-      <p>{weatherData.current.condition.text}</p>
-      <img
-        src={`https:${weatherData.current.condition.icon}`}
-        alt={weatherData.current.condition.text}
-      />
+    <div className="flex justify-center mt-6">
+      <div className="text-center border border-white rounded-lg p-6 max-w-sm w-full bg-opacity-20 backdrop-blur-md bg-white/20 dark:bg-gray-800/30">
+
+        <h2 className="text-2xl font-semibold text-white mb-2">
+          {weatherData.location.name}, {weatherData.location.country}
+        </h2>
+        <p className="text-3xl text-white mb-2">{weatherData.current.temp_c}°C</p>
+        
+        <p className="text-white mb-4">{weatherData.current.condition.text}</p>
+           
+        <img
+          src={`https:${weatherData.current.condition.icon}`}
+          alt={weatherData.current.condition.text}
+          className="mx-auto"
+        />
+      </div>
     </div>
   );
 };
